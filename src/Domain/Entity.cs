58ef3b0b36f7ebf69b1838571d69042baa335018
thiribeironet <thribeiro.dev@gmail.com>
2023-10-14 15:23:53 -0300

    Create Category and Product

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity() 
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; }
+    public DateTime EditedOn { get; set; }
+}
