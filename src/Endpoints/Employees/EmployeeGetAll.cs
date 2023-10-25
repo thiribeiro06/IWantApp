9f587d30ab882bac6ea1e61516601334f2581bab
thiribeiro06 <thribeiro.dev@gmail.com>
2023-10-25 08:06:52 -0300

    Refactoring pagination using Dapper

--- src/Endpoints/Employees/EmployeeGetAll.cs
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,6 +1,8 @@
-﻿using IWantApp.Infra.Data;
+﻿using Dapper;
+using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing.Template;
+using Microsoft.Data.SqlClient;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Employees;
@@ -11,18 +13,27 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
-        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-        var response = new List<EmployeeResponse>();
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
 
-        foreach(var item in users)
-        {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            response.Add(new EmployeeResponse(item.Email, userName));
-        }
+        var query =
+            @"SELECT AU.Email
+                   , AC.ClaimValue AS Name
+                FROM AspNetUsers AU
+               INNER JOIN AspNetUserClaims AC
+                  ON AU.Id = AC.UserId
+                 AND AC.ClaimType = 'Name'
+               ORDER BY Name
+              OFFSET (@page - 1) * @rows ROWS
+               FETCH NEXT  @rows 
+                ROWS ONLY";
+
+        var response = db.Query<EmployeeResponse>(
+            query,
+            new {page, rows}
+            );
+
         return Results.Ok(response);
     }
 }
