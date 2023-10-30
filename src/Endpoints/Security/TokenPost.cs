e32954b13cfdb6dbe8d3de046972bb155cde33d3
thiribeiro06 <thribeiro.dev@gmail.com>
2023-10-30 06:55:22 -0300

    Initiating token generation

---
+++ src/Endpoints/Security/TokenPost.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IWantApp.Endpoints.Security;
+
+public class TokenPost
+{
+    public static string Template => "/token";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(LoginRequest loginRequest,UserManager<IdentityUser> userManager)
+    {
+        var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
+        if (user == null || !userManager.CheckPasswordAsync(user, loginRequest.Password).Result) 
+        {
+            Results.BadRequest();
+        }
+
+        var key = Encoding.ASCII.GetBytes("wjifuhwauhARWWROKN@24");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Email, loginRequest.Email),
+            }),
+            SigningCredentials =
+                new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Audience = "IWantApp",
+            Issuer = "Issuer"
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return Results.Ok(new
+        {
+            token = tokenHandler.WriteToken(token),
+        });
+
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 using IWantApp.Endpoints.Categories;
 using IWantApp.Endpoints.Employees;
+using IWantApp.Endpoints.Security;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
@@ -28,5 +29,6 @@ app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.H
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
 app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methods, EmployeeGetAll.Handle);
+app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 
 app.Run();
